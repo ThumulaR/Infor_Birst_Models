bebfab23f11477f32c796ca7b868cde439bcc99b
ThumulaR <130494506+ThumulaR@users.noreply.github.com>
2025-08-12 16:13:07 +0530

    Stage 1
    Remove unwanted conditions : Month, Whse
    Add DatePart

--- K_Test_Inqube_Summary_Outbound.sql
+++ K_Test_Inqube_Summary_Outbound.sql
@@ -1,6 +1,5 @@
-
-
 SELECT 
+	FORMAT(Final.date, 'MM-yyyy') AS DatePart,
 	Final.date,
 	SUM(Final.SKU_Count) AS count
 FROM
@@ -8,14 +7,7 @@ FROM
 SELECT	
 	WaveOrder.date,
 	WaveOrder.wavekey,COUNT(DISTINCT WaveOrder.sku) AS SKU_Count
---	WaveOrder.wave_orderkey,
---	COUNT(DISTINCT WaveOrder.sku)
---	WaveOrder.sku AS wave_sku,
---	WaveOrder.order_orderkey,
---	WaveOrder.orderlinenumber,
---	itrn.sku AS itran_sku,
---	SUM(WaveOrder.shippedqty)
---	WaveOrder.effectivedate
+
 FROM
 (
 	SELECT	
@@ -32,11 +24,10 @@ FROM
 		SELECT CAST(DATEADD(minute, 30, DATEADD(hour, 5, w.ext_udf_date1 )) AS DATE) AS date,w.wavekey,w.batchordernumber,wd.orderkey
 		FROM V{=Replace(GetVariable('p_SCHEMA'),'\'','')}.wave w
 		INNER JOIN V{=Replace(GetVariable('p_SCHEMA'),'\'','')}.wavedetail wd	ON w.wavekey = wd.wavekey
-		WHERE w.whseid = 'wmwhse2' 
-			AND w.batchordernumber <> ''
+		WHERE 
+			w.batchordernumber <> ''
 			AND w.ext_udf_date1 IS NOT NULL
 			AND w.batchordernumber IS NOT NULL
-			AND CAST(DATEADD(minute, 30, DATEADD(hour, 5, w.ext_udf_date1 )) AS DATE) between '2025-05-01' and '2025-05-31' 
 	)waveData	
 	
 	INNER JOIN (
@@ -59,4 +50,3 @@ INNER JOIN(
 GROUP BY WaveOrder.wavekey,WaveOrder.date
 )Final
 GROUP BY Final.date
---ORDER BY Final.date
