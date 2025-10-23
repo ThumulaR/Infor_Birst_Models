0676f93d3837f3db3558f4c449980335e24f0739
ThumulaR <130494506+ThumulaR@users.noreply.github.com>
2025-10-23 22:56:38 +0530

    data missing issue corrected version

--- K_SingerNormalUnloadingDetail.sql
+++ K_SingerNormalUnloadingDetail.sql
@@ -1,27 +1,62 @@
-SELECT 
-    FORMAT(Cal_Date_, 'MM-yyyy') AS DatePart,
-    Cal_Date_,
-	VehicleNumber,
-    ContainerNo,
-    --TrailerNo,
-    COUNT(NULLIF(CUI_40FT, '')) AS CUI_40FT,
-    COUNT(NULLIF(CUI_20FT, '')) AS CUI_20FT,
-    COUNT(NULLIF(CUI_LCL, '')) AS CUI_LCL
-FROM 
+SELECT
+	FORMAT(summary.Cal_Date, 'MM-yyyy') AS DatePart,
+	summary.Cal_Date AS Cal_Date_,
+	summary.VehicleNumber,
+	summary.ContainerNo,
+	summary.CUI_20FT,
+	summary.CUI_40FT,
+	summary.CUI_LCL
+FROM
 (
-    SELECT 
-        CAST(DATEADD(minute, 30, DATEADD(hour, 5, c.CHARGE_DATE)) AS DATE) AS Cal_Date_,
-        c.CHARGE_CODE AS ChargeCode,
-        r.VEHICLENUMBER 'VehicleNumber',
-		r.CONTAINERKEY 'ContainerNo',
-		--r.TrailerNumber 'TrailerNo',		
-        IIF(c.CHARGE_CODE = 'SIN-N-CUI-20FT', c.CHARGE_CODE, '') AS CUI_20FT,
-        IIF(c.CHARGE_CODE = 'SIN-N-CUI-40FT', c.CHARGE_CODE, '') AS CUI_40FT,
-        IIF(c.CHARGE_CODE = 'SIN-N-CUI-LCL', c.CHARGE_CODE, '') AS CUI_LCL
-    FROM BILLADMIN.BIC_CHARGE c
+	SELECT 
+		CAST(DATEADD(minute, 30, DATEADD(hour, 5, c.CHARGE_DATE)) AS DATE) AS Cal_Date,
+		r.VEHICLENUMBER AS VehicleNumber,
+		r.CONTAINERKEY AS ContainerNo,
+		COUNT(c.CHARGE_CODE) AS  CUI_20FT,
+		0 AS CUI_40FT,
+		0 AS  CUI_LCL
+		
+	FROM BILLADMIN.BIC_CHARGE c
 	Left join V{=Replace(GetVariable('p_SCHEMA'), '\'', '')}.receipt r ON c.order_no = r.RECEIPTKEY
-    WHERE c.CHARGE_CODE IN ('SIN-N-CUI-20FT', 'SIN-N-CUI-40FT', 'SIN-N-CUI-LCL')
+	WHERE c.CHARGE_CODE IN ('SIN-N-CUI-20FT')
 	AND c.DELETE_FLAG = 0
+	GROUP BY CAST(DATEADD(minute, 30, DATEADD(hour, 5, c.CHARGE_DATE)) AS DATE),
+			 r.VEHICLENUMBER,
+			 r.CONTAINERKEY
+			 
+	UNION ALL		 
+			 
+	SELECT 
+		CAST(DATEADD(minute, 30, DATEADD(hour, 5, c.CHARGE_DATE)) AS DATE) AS Cal_Date,
+		r.VEHICLENUMBER AS VehicleNumber,
+		r.CONTAINERKEY AS ContainerNo,
+		0 AS  CUI_20FT,
+		COUNT(c.CHARGE_CODE) AS  CUI_40FT,
+		0 AS  CUI_LCL
+		
+	FROM BILLADMIN.BIC_CHARGE c
+	Left join V{=Replace(GetVariable('p_SCHEMA'), '\'', '')}.receipt r ON c.order_no = r.RECEIPTKEY
+	WHERE c.CHARGE_CODE IN ('SIN-N-CUI-40FT')
+	AND c.DELETE_FLAG = 0
+	GROUP BY CAST(DATEADD(minute, 30, DATEADD(hour, 5, c.CHARGE_DATE)) AS DATE),
+			 r.VEHICLENUMBER,
+			 r.CONTAINERKEY
 
-) Summary
-GROUP BY Cal_Date_, VehicleNumber, ContainerNo
+	UNION ALL
+
+	SELECT 
+		CAST(DATEADD(minute, 30, DATEADD(hour, 5, c.CHARGE_DATE)) AS DATE) AS Cal_Date,
+		r.VEHICLENUMBER AS VehicleNumber,
+		r.CONTAINERKEY AS ContainerNo,
+		0 AS  CUI_20FT,
+		0 AS  CUI_40FT,
+		COUNT(c.CHARGE_CODE) AS  CUI_LCL
+		
+	FROM BILLADMIN.BIC_CHARGE c
+	Left join V{=Replace(GetVariable('p_SCHEMA'), '\'', '')}.receipt r ON c.order_no = r.RECEIPTKEY
+	WHERE c.CHARGE_CODE IN ('SIN-N-CUI-LCL')
+	AND c.DELETE_FLAG = 0
+	GROUP BY CAST(DATEADD(minute, 30, DATEADD(hour, 5, c.CHARGE_DATE)) AS DATE),
+			 r.VEHICLENUMBER,
+			 r.CONTAINERKEY	
+)summary
